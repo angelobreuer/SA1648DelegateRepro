ad14ce9ad9749b45fd6e48c71e2e793b95fffb9e
Angelo Breuer <angelobr@outlook.de>
2021-01-17 14:28:18 +0100

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿// <copyright file="Program.cs" company="PlaceholderCompany">
+//     Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace SA1648DelegateRepro
+{
+    using System.Threading;
+
+    /// <summary>
+    ///     D.
+    /// </summary>
+    internal class Program
+    {
+        /// <summary>
+        ///     A.
+        /// </summary>
+        /// <returns>B.</returns>
+        /// <include file="docs.xml" path="Root/Documentation[@name='CancellationToken']/*"/>
+        public delegate bool A(CancellationToken cancellationToken = default);
+    }
+}
